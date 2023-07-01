ebac84609611f96eaaf8f5bbe1329d689d43f38b
Yefree Valdez <yefree13santos@outlook.com>
2023-07-01 12:43:34 +0200

    pushing final changes

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -2,42 +2,34 @@
 
 const peleas = [
   {
-    boxer1: "Andre Ward",
-    boxer2: "Mikael Tyson",
-    date: "Martes 7 Jul, 2023.",
-    description: "Pelea de peso completo",
-    place: "Los Angeles California, EEUU.",
-    image: "../public/images/article-images/article-fight-img.webp",
+    boxer1: 'Andre Ward',
+    boxer2: 'Mikael Tyson',
+    date: 'Martes 7 Jul, 2023.',
+    description: 'Pelea de peso completo',
+    place: 'Los Angeles California, EEUU.',
+    image: '../public/images/article-images/article-fight-img.webp',
   },
   {
-    boxer1: "Jackie Chan",
-    boxer2: "Mera Cruz",
-    date: "Martes 7 Jul, 2023.",
-    description: "Pelea de pesoligreo",
-    place: "Los Angeles California, EEUU.",
-    image: "../public/images/article-images/fight-2.webp",
+    boxer1: 'Jackie Chan',
+    boxer2: 'Mera Cruz',
+    date: 'Martes 7 Jul, 2023.',
+    description: 'Pelea de pesoligreo',
+    place: 'Los Angeles California, EEUU.',
+    image: '../public/images/article-images/fight-2.webp',
   },
   {
-    boxer1: "Ruslav Molkinivking",
-    boxer2: "Antonio Machado",
-    description: "Pelea de pesoligreo",
-    date: "Martes 7 Jul, 2023.",
-    place: "Los Angeles California, EEUU.",
-    image: "../public/images/article-images/article-fight-img.webp",
-  },
-  {
-    boxer1: "Xi Xiaonj",
-    boxer2: "Julio Gustico",
-    description: "Pelea de pesoligreo",
-    date: "Martes 7 Jul, 2023.",
-    place: "Los Angeles California, EEUU.",
-    image: "../public/images/article-images/article-fight-img.webp",
+    boxer1: 'Ruslav Molkinivking',
+    boxer2: 'Antonio Machado',
+    description: 'Pelea de pesoligreo',
+    date: 'Martes 7 Jul, 2023.',
+    place: 'Los Angeles California, EEUU.',
+    image: '../public/images/article-images/article-fight-img.webp',
   },
 ];
 
-const peleasContenedor = document.querySelector("#fights-container");
+const peleasContenedor = document.querySelector('#fights-container');
 
-window.addEventListener("DOMContentLoaded", function () {
+window.addEventListener('DOMContentLoaded', function () {
   let mostrarPeleas = peleas.map((pelea) => {
     return ` <article class="peleas__article">
             <img
@@ -54,19 +46,19 @@ window.addEventListener("DOMContentLoaded", function () {
             </p>
           </article>`;
   });
-  mostrarPeleas = mostrarPeleas.join("");
+  mostrarPeleas = mostrarPeleas.join('');
   peleasContenedor.innerHTML = mostrarPeleas;
 });
 
-const nav = document.querySelector(".nav");
+const nav = document.querySelector('.nav');
 
-window.addEventListener("scroll", function () {
+window.addEventListener('scroll', function () {
   const scrollHeight = window.pageYOffset;
   const navHeight = nav.getBoundingClientRect().height;
   if (scrollHeight > navHeight) {
-    nav.classList.add("fixed-nav");
+    nav.classList.add('fixed-nav');
   } else {
-    nav.classList.remove("fixed-nav");
+    nav.classList.remove('fixed-nav');
   }
 });
 
