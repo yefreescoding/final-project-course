a0c20c659c09ceb1876a230a0871f50fc526cffc
yefreescoding <yefree13santos@outlook.com>
2023-07-03 19:00:33 +0200

    minor ui design fixed

--- src/javascript/Data/boxers.js
+++ src/javascript/Data/boxers.js
@@ -1,7 +1,7 @@
 const boxers = [
   {
     name: "Vasily Lomachenko",
-    record: "48-6-1",
+    record: "17-3-0",
     country: "Ucrania",
     bio: `Es un boxeador profesional ucraniano; medallista olimpico de oro en los 
     Juegos Olímpicos de Pekín 2008 en la división de peso pluma y los Juegos Olímpicos
@@ -17,11 +17,11 @@ const boxers = [
     image: "./public/images/boxers-images/gervonta-davis.jpg",
   },
   {
-    name: "Jhon Doe",
-    record: "48-6-1",
+    name: "Oleksandr Usyk",
+    record: "20-0-0",
     country: "Ucrania",
-    bio: `Es un boxeador profesional ucraniano; fue campeón mundial pluma de la WBO y campeón mundial superpluma de la WBO, fue también campeón mundial de la WBA, WBO, y WBC Peso Ligero hasta el 17 de octubre de 2020, fecha en la que cayó por decisión unánime ante Teofimo López, Ganó la medalla de plata en el Campeonato de Boxeo Amateur del Mundo de 2007, medallas de oro en los Campeonatos de Boxeo Amateur del Mundo de 20092​ y el Campeonato de Boxeo Amateur del Mundo 2011, así como la medalla de oro en los Juegos Olímpicos de Pekín 2008 en la división de peso pluma y los Juegos Olímpicos de Londres 2012 en la división de peso ligero. Es considerado por The Ring como uno de los mejores boxeadores libra por libra del momento`,
-    image: "./public/images/boxers-images/vasiliy-lomachenko.png",
+    bio: `Campeón mundial en dos categorías de peso, habiendo ostentado los títulos unificados de peso pesado de la AMB (Super), la FIB, la OMB y la IBO; y el título indiscutible de peso crucero de 2018 a 2019, siendo el primer campeón de peso crucero en poseer los cuatro títulos mundiales principales. Es ampliamente considerado como uno de los mejores boxeadores ucranianos de todos los tiempos.`,
+    image: "./public/images/boxers-images/usyk.webp",
   },
   {
     name: "Vasily Lomachenko",
--- src/javascript/Data/fights.js
+++ src/javascript/Data/fights.js
@@ -1,5 +1,13 @@
 const peleas = [
   {
+    boxer1: "Naoya Inoue",
+    boxer2: "Stephen Fulton",
+    description: "Pelea de pesoligreo",
+    date: "Martes 24 Jul, 2023.",
+    place: "Osaka, Japon.",
+    image: "public/images/article-images/poster-hero.avif",
+  },
+  {
     boxer1: "Andre Ward",
     boxer2: "Mikael Tyson",
     date: "Martes 7 Jul, 2023.",
@@ -14,13 +22,5 @@ const peleas = [
     description: "Pelea de pesoligreo",
     place: "Los Angeles California, EEUU.",
     image: "public/images/article-images/fight-2.webp",
-  },
-  {
-    boxer1: "Ruslav Molkinivking",
-    boxer2: "Antonio Machado",
-    description: "Pelea de pesoligreo",
-    date: "Martes 7 Jul, 2023.",
-    place: "Los Angeles California, EEUU.",
-    image: "public/images/article-images/article-fight-img.webp",
   },
 ];
