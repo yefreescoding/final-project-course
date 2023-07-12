70840da66f2c04368d628f15dd140d1c8de4e3ff
yefreescoding <yefree13santos@outlook.com>
2023-07-12 20:42:19 +0200

    add arena sql database

--- database/arena.sql
+++ database/arena.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-07-2023 a las 20:39:52
+-- Tiempo de generación: 12-07-2023 a las 19:12:22
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -45,9 +45,9 @@ CREATE TABLE `boxers` (
 INSERT INTO `boxers` (`id`, `name`, `weight`, `record`, `country`, `p4p_ranking`) VALUES
 (1, 'Anthony Joshua', 240, '24-1-0', 'United Kingdom', 13),
 (2, 'Tyson Fury', 260, '30-0-1', 'United Kingdom', 16),
-(3, 'Canelo Alvarez', 168, '56-1-2', 'Mexico', 1),
-(4, 'Terence Crawford', 147, '37-0-0', 'USA', 2),
-(5, 'Naoya Inoue', 118, '21-0-0', 'Japan', 3),
+(3, 'Canelo Alvarez', 168, '56-1-2', 'Mexico', 5),
+(4, 'Terence Crawford', 147, '37-0-0', 'USA', 3),
+(5, 'Naoya Inoue', 118, '21-0-0', 'Japan', 2),
 (6, 'Errol Spence Jr.', 147, '27-0-0', 'USA', 4),
 (7, 'Teofimo Lopez', 135, '16-0-0', 'Honduras', 5),
 (8, 'Gennady Golovkin', 160, '41-1-1', 'Kazakhstan', 6),
@@ -55,7 +55,18 @@ INSERT INTO `boxers` (`id`, `name`, `weight`, `record`, `country`, `p4p_ranking`
 (10, 'Josh Taylor', 140, '18-0-0', 'Scotland', 8),
 (11, 'Manny Pacquiao', 147, '62-7-2', 'Philippines', 9),
 (12, 'Vasyl Lomachenko', 135, '14-2-0', 'Ukraine', 10),
-(13, 'Stephen Fulton', 122, '21-0-0', 'USA', 20);
+(13, 'Stephen Fulton', 122, '21-0-0', 'USA', 20),
+(14, 'Jake Paul', 186, '6-1-0', 'United States', 99),
+(15, 'Nate Diaz', 184, '0-0-0', 'United States', 999),
+(16, 'Emanuel Navarrete', 130, '37-1-0', 'Mexico', 17),
+(17, 'Óscar Valdez', 130, '30-1-0', 'Mexico', 24),
+(18, 'Dillan Whyte', 247, '29-3-0', 'Reino unido', 20),
+(19, 'Daniel Dubois', 245, '19-1-0', 'Reino Unido', 17),
+(20, 'Oleksander Usyk', 245, '20-0-0', 'Ucrania', 1),
+(21, 'Dmitry Bivol', 165, '21-0-0', 'Rusia', 6),
+(22, 'Artur Beterbiev', 168, '19-0-0', 'Rusia', 19),
+(23, 'Callum Smith', 168, '20-0-0', 'Reino Unido', 29),
+(24, 'Jermell Charlo', 150, '35-1-1', 'Estados Unidos', 18);
 
 -- --------------------------------------------------------
 
@@ -76,8 +87,14 @@ CREATE TABLE `fights` (
 --
 
 INSERT INTO `fights` (`id`, `boxer1_id`, `boxer2_id`, `date`, `place`) VALUES
-(1, 7, 9, '2023-07-15', 'Las Vegas, Nevada. USA.'),
-(2, 5, 13, '2023-07-17', 'Tokyo, Japon.');
+(1, 14, 15, '2023-08-05', 'Dallas, Texas. USA.'),
+(2, 5, 13, '2023-07-25', 'Tokyo, Japon.'),
+(3, 6, 4, '2023-07-29', 'Las Vegas, Nevada. USA.'),
+(4, 16, 17, '2023-08-12', 'Glendale, Arizona. USA.'),
+(5, 1, 18, '2023-08-12', 'Londres, Inglaterra.'),
+(6, 20, 19, '2023-08-26', 'Wroclaw, Poland.'),
+(7, 22, 23, '2023-08-19', 'Quebec, Canada.'),
+(8, 3, 24, '2023-09-30', 'Las Vegas, Nevada. USA.');
 
 --
 -- Índices para tablas volcadas
@@ -105,13 +122,13 @@ ALTER TABLE `fights`
 -- AUTO_INCREMENT de la tabla `boxers`
 --
 ALTER TABLE `boxers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de la tabla `fights`
 --
 ALTER TABLE `fights`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Restricciones para tablas volcadas
